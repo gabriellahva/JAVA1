abb5af8b3ff24f1e6ce6cfb1a404a2d56fb45ed5
gabriellahva <gaby.holanda15@gmail.com>
2024-03-14 14:55:38 -0300

    Arquivos de Aula Java

---
+++ Programa3/src/programa3/Programa3.java
@@ -0,0 +1,32 @@
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programa3;
+
+/**
+ *
+ * @author Aluno CA
+ */
+public class Programa3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    Scanner scanner = new 
+    Scanner (System.in);
+        
+        System.out.println ("Digite a distancia total percorrida em km");
+        double distancia = scanner.next.Double();
+        
+        System.out.println ("Digite o total de combustivel gasto em litros");
+        double distancia = scanner.next.Double();
+        
+        
+        
+    }
+    
+}
---
+++ Programa4/src/programa4/Programa4.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programa4;
+
+/**
+ *
+ * @author Aluno CA
+ */
+public class Programa4 {
+    public static void main(String[] args) {
+        double dp = 550;
+        double cg = 50; 
+        double cm = dp / cg;
+        
+        System.out.println("O consumo médio do automóvel é de " + cm + " km/l.");
+    }
+}
