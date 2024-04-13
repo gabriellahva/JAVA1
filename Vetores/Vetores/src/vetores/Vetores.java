7e0098c70f2fb1bc35eafeed768fe8a29462f385
gabriellahva <163450472+gabriellahva@users.noreply.github.com>
2024-04-12 23:10:34 -0300

    Add files via upload

---
+++ Vetores/Vetores/src/vetores/Vetores.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vetores;
+import java.util.Scanner;
+
+/**
+ *
+ * @author pc
+ */
+public class Vetores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Entender o Arrays
+        int numero [] = new int[5];
+        Scanner entrada = new Scanner (System.in);
+        
+        //recebendo Numeros
+        for (int indice=0; indice<5;indice++)
+        {
+            System.out.println("Digite um Numero: ");
+            numero[indice] = entrada.nextInt();
+        }
+        
+        // Imprimir valores do Vetor
+              
+    }
+    
+}
+
+
+   
